9dbc97cb839c9f5e1e1b621759a2a9e4fcee3eb2
Lucas Le <lucas.lpnl@byteuptech.com>
2025-03-01 07:58:03 +0700

    try to fix github actions

--- next.config.mjs
+++ next.config.mjs
@@ -3,14 +3,23 @@ import createNextIntlPlugin from 'next-intl/plugin';
 
 const withMDX = mdx({
     extension: /\.mdx?$/,
-    options: { },
+    options: {},
 });
 
 const withNextIntl = createNextIntlPlugin();
 
-/** @type {import('next').NextConfig} */
+/**
+ * @type {import('next').NextConfig}
+ */
 const nextConfig = {
     pageExtensions: ['ts', 'tsx', 'md', 'mdx'],
 };
 
-export default withNextIntl(withMDX(nextConfig));
+export default function config() {
+    try {
+        return withNextIntl(withMDX(nextConfig));
+    } catch (error) {
+        console.error('Error in Next.js config:', error);
+        throw new Error(error instanceof Error ? error.message : String(error));
+    }
+}
