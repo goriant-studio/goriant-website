1350733226bbb8dd3417e655b2b28ddd1250e947
Lucas Le <lucas.lpnl@byteuptech.com>
2025-03-01 12:20:58 +0700

    reinit project

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,16 @@
  */
 const nextConfig = {
   output: 'export',
+  // The subpath where the app is deployed
+  basePath: '/goriant-website',
+  
+  // The prefix for all static assets
+  assetPrefix: '/goriant-website/',
+  
+  // If you’re using the new app router and static exports, you may also need:
+  images: {
+    unoptimized: true,
+  },
  
   // Optional: Change links `/me` -> `/me/` and emit `/me.html` -> `/me/index.html`
   // trailingSlash: true,
