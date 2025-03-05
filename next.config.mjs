dbf37b2fd922b2671bc39b6f614958d2d69a8098
Lucas Le <lucas.lpnl@byteuptech.com>
2025-03-05 08:57:06 +0700

    Fix image path

---
+++ next.config.mjs
@@ -0,0 +1,28 @@
+import { PHASE_DEVELOPMENT_SERVER } from "next/constants.js";
+
+export default (phase) => {
+  const isDev = phase === PHASE_DEVELOPMENT_SERVER;
+
+  /**
+   * @type {import('next').NextConfig}
+   */
+  const nextConfig = {
+    output: "export",
+    distDir: "dist",
+    images: {
+      unoptimized: true,
+    },
+    basePath: "",
+    assetPrefix: isDev ? undefined : "https://cdn.mydomain.com",
+    trailingSlash: true,
+    // Cấu hình compiler để loại bỏ server-only code
+    compiler: {
+      removeConsole: process.env.NODE_ENV === "production",
+    },
+    experimental: {
+      appDir: false,
+    },
+  };
+
+  return nextConfig;
+};
